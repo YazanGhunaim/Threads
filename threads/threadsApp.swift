ba9d2c9bf7ae51c52f445330e32ae45b40fe21b2
Yazan Ghunaim <yazanghunaim07@gmail.com>
2023-08-23 17:48:10 +0300

    setting up directory

---
+++ threads/threadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  threadsApp.swift
+//  threads
+//
+//  Created by Yazan Ghunaim on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct threadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
